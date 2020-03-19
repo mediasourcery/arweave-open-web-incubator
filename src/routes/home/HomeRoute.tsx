fbfb1c99ca3df2c4cf7e0f5f72989bfd71ef9891
Chris Tate <chris.tate@mediasourcery.com>
2020-03-19 18:05:01 -0500

    fixes redirect_uri

--- src/routes/home/HomeRoute.tsx
+++ src/routes/home/HomeRoute.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 import { Redirect } from '@reach/router';
 
 export const HomeRoute: React.FC = () => (
-  <Redirect to={`${process.env.PUBLIC_URL}upload`} />
+  <Redirect to={`${process.env.PUBLIC_URL}documents`} />
 );
